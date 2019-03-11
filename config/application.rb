05ab5638cc7d5be6497229c782e045c39097d557
rogerdunnGA <rogerdunn@Gmail.com>
2019-03-11 11:34:11 -0400

    updated application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require 'action_controller/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module 360-Review
+module RailsApi360Review
   # :nodoc:
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified
